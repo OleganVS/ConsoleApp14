a30721576a585aec575a86268b8cb80cbb1fc9c1
OleganVS <118183808+OleganVS@users.noreply.github.com>
2022-11-25 16:59:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp14.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14._2
+{
+    internal class Program
+    {
+        static double GetGeron(double x, double y, double z)
+        { 
+            double p = (x + y + z)/2;
+            double s = Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длины сторон первого треугольника");
+            double x1 = Convert.ToDouble(Console.ReadLine()); 
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double z1 = Convert.ToDouble(Console.ReadLine());
+            double s1 = GetGeron(x1, y1, z1);
+            Console.WriteLine("Введите длины сторон второго треугольника");
+            double x2 = Convert.ToDouble(Console.ReadLine()); 
+            double y2 = Convert.ToDouble(Console.ReadLine()); 
+            double z2 = Convert.ToDouble(Console.ReadLine());
+            double s2 = GetGeron(x2, y2, z2);
+            Console.WriteLine("s1 = {o}, s2 = {1}", GetGeron (x1, y1, z1), GetGeron(x2, y2, z2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void GetParamsCube(double h, out double v, out double s)
+        {
+            v = h * h * h;
+            s = 6 * h * h;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите высоту ребра куба");
+            double h = Convert.ToDouble(Console.ReadLine());
+            double s;
+            double v;
+            GetParamsCube (h, out s, out v);
+            Console.WriteLine("{0} {1}" ,s,v);
+            Console.ReadKey();
+
+        }
+    }
+}
